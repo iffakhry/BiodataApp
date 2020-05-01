148315d6461b69ed5068b324c523f3c0538a5cfe
Fakhry Macbook <fakhry.ikhsan.firdaus@gmail.com>
2020-05-01 21:49:23 +0700

    (+) fixed some bugs (+) add comments

--- app/src/main/java/id/smkcoding/biodataapp/MainActivity.kt
+++ app/src/main/java/id/smkcoding/biodataapp/MainActivity.kt
@@ -9,6 +9,7 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
+    //inisialisasi variabel global untuk menampung data inputan user
     private var namaInput : String = ""
     private var emailInput : String = ""
     private var telpInput : String = ""
@@ -19,18 +20,24 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        btnSave.setOnClickListener { validasiInput() }
-
+        //set data spinner Gender
         setDataSpinnerGender()
+
+        //memberi click listener ke tombol Save.
+        //jika tombol save ditekan, maka akan menjalankan method validasiInput()
+        btnSave.setOnClickListener { validasiInput() }
     }
 
+    //fungsi untuk set data pada spinner gender dengan string-array jenis kelamin
     private fun setDataSpinnerGender() {
         val adapter = ArrayAdapter.createFromResource(this, R.array.jenis_kelamin, android.R.layout.simple_spinner_item)
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
         spinnerGender.adapter = adapter
     }
 
+    //fungsi untuk melakukan validasi input
     private fun validasiInput() {
+        //mendapatkan value/data dari tiap EditText dan menyimpannya ke dalam variabel yang telah dibuat
         namaInput = edtName.text.toString()
         emailInput = edtEmail.text.toString()
         telpInput = edtTelp.text.toString()
@@ -38,23 +45,28 @@ class MainActivity : AppCompatActivity() {
         genderInput = spinnerGender.selectedItem.toString()
 
         when{
-
+            //cek di tiap inputan apakah kosong atau tidak, jika kosong maka tampilkan error
             namaInput.isEmpty() -> edtName.error = "Nama tidak boleh kosong"
-            genderInput.equals("Pilih Kelamin", ignoreCase = true) -> tampilToast("Jenis Kelamin harus dipilih")
+            genderInput.equals("Pilih Jenis Kelamin", ignoreCase = true) -> tampilToast("Jenis Kelamin harus dipilih")
             emailInput.isEmpty() -> edtEmail.error = "Email tidak boleh kosong"
             telpInput.isEmpty() -> edtTelp.error = "Telp tidak boleh kosong"
             alamatInput.isEmpty() -> edtAddress.error = "Alamat tidak boleh kosong"
 
             else -> {
-
+                //jika semua inputan telah diisi, maka jalankan method goToProfilActivity
                 tampilToast("Navigasi ke halaman profil diri")
-                navigasiKeProfilDiri()
+                goToProfilActivity()
             }
         }
-
     }
 
-    private fun navigasiKeProfilDiri() {
+    //fungsi untuk menampilkan toast
+    private fun tampilToast(message: String) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+    }
+
+    //fungsi untuk berpindah ke ProfilActivity, sekaligus mengirimkan data hasil input user
+    private fun goToProfilActivity() {
         val intent = Intent(this, ProfilActivity::class.java)
 
         val bundle = Bundle()
@@ -66,11 +78,6 @@ class MainActivity : AppCompatActivity() {
 
         intent.putExtras(bundle)
 
-
         startActivity(intent)
-    }
-
-    private fun tampilToast(message: String) {
-        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
     }
 }
--- app/src/main/java/id/smkcoding/biodataapp/ProfilActivity.kt
+++ app/src/main/java/id/smkcoding/biodataapp/ProfilActivity.kt
@@ -12,6 +12,7 @@ import kotlinx.android.synthetic.main.activity_profil.*
 class ProfilActivity : AppCompatActivity() {
 
     companion object {
+        //set variabel REQUEST_CODE = 100
         val REQUEST_CODE  = 100
     }
 
@@ -20,23 +21,31 @@ class ProfilActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_profil)
 
+        //memanggil fungsi ambilData
         ambilData()
 
+        //memberi click listener ke tombol edit.
+        //jika tombol edit name ditekan, maka akan menjalankan method navigasiKeEditProfil()
         btnEditName.setOnClickListener { navigasiKeEditProfil() }
+
+        //memberi click listener ke tomboll call
         btnCall.setOnClickListener { dialPhoneNumber(txtTelp.text.toString()) }
 
 
     }
 
+
     private fun ambilData() {
         val bundle = intent.extras
 
+        //menangkap data yang dikirimkan oleh MainActivity lalu menyimpannya ke sebuah variabel
         val nama = bundle.getString("nama")
         val gender = bundle.getString("gender")
         val email = bundle.getString("email")
         val telp = bundle.getString("telp")
         val alamat = bundle.getString("alamat")
 
+        //set tulisan di tiap TextView dengan data yang kita dapatkan sebelumnya.
         txtName.text = nama
         txtGender.text = gender
         txtEmail.text = email
@@ -45,27 +54,32 @@ class ProfilActivity : AppCompatActivity() {
 
     }
 
+    //fungsi untuk berpindah ke EditProfilActivity
     private fun navigasiKeEditProfil() {
         val intent = Intent(this, EditProfilActivity::class.java)
 
+        // mengirimkan data dengan keyName "nama"
         val namaUser = txtName.text.toString()
         intent.putExtra("nama", namaUser)
 
-//        startActivity(intent)
         startActivityForResult(intent, REQUEST_CODE)
     }
 
+    //fungsi untuk menangkap hasil dari proses startActivityForResult
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         if (requestCode == REQUEST_CODE){
             if (resultCode == Activity.RESULT_OK) {
+                //jika sukses maka data hasil edit kita tampikan ke TextView txtName
                 val result = data?.getStringExtra("nama")
                 txtName.text = result
             }else{
+                //jika gagal maka tampilkan toast
                 Toast.makeText(this, "Edit failed", Toast.LENGTH_SHORT).show()
             }
         }
     }
 
+    //fungsi untuk melakukan dial
     private fun dialPhoneNumber(phoneNumber: String) {
         val intent = Intent(Intent.ACTION_DIAL).apply {
             data = Uri.parse("tel:$phoneNumber")
